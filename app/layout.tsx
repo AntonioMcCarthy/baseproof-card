aaaf3eb9b39c154426d0465fa8a2337baacae497
AntonioMcCarthy <242352163+AntonioMcCarthy@users.noreply.github.com>
2026-07-23 16:19:34 +0800

    Add Talent App verification meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="base:app_id" content="6a61c75c078f6baf9ef3047f" />
+        <meta
+          name="talentapp:project_verification"
+          content="19c1a0e7252ea68e7ae6932c8c321e4359cf50c2d7cc420a5caf8d05a23d10259411cf9714013b0b7fd022fa5996b8fc4067501cf5f3955c55e3a1a9359dcfdb"
+        />
       </head>
       <body>
         <Providers>{children}</Providers>
